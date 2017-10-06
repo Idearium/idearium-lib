4121fbd7eec0a5b5314bc43c659f9840fdcdd645
Allan Chau <allan.chau@icloud.com>
2017-10-06 15:40:13 +1030

    Attempt to fix tests timing out

--- idearium-lib/test/scripts/test.sh
+++ idearium-lib/test/scripts/test.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env sh
 
-DEBUG=lib:logger:*,idearium-lib:common:exception BLUEBIRD_WARNINGS=0 "/app/node_modules/.bin/ava" --timeout=15s --verbose ./test/*.ava.js
+DEBUG=lib:logger:*,idearium-lib:common:exception BLUEBIRD_WARNINGS=0 "/app/node_modules/.bin/ava" --timeout=15s --verbose --concurrency=1 ./test/*.ava.js

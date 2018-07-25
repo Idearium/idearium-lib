0990855d9adbfa88796ec1204ecb3da7288209ac
Allan Chau <allan.chau@icloud.com>
2018-07-25 11:59:20 +0930

    Prevent config loading errors

--- middleware/log-error.js
+++ middleware/log-error.js
@@ -1,6 +1,5 @@
 'use strict';
 
-const config = require('../common/config');
 const log = require('../common/log')('idearium-lib:middleware:log-error');
 
 module.exports = (err, req, res, next) => {
@@ -8,29 +7,11 @@ module.exports = (err, req, res, next) => {
     // Let's move on straight away.
     next(err);
 
-    // The duration of the request.
-    const duration = Date.now() - (req.responseStart || Date.now());
-
-    // The packet we're going to log.
-    const packet = {
+    log.error({
+        duration: Date.now() - (req.responseStart || Date.now()),
         err,
-        production: config.get('production'),
         req,
-        responseTime: duration,
-        status: res.statusCode,
-    };
-
-    // A short version of the user, if it exists.
-    if (req.user && req.user.username && req.user._id) {
-
-        packet.user = {
-            id: req.user._id,
-            username: req.user.username,
-        };
-
-    }
-
-    // Log the error.
-    log.error(packet);
+        res,
+    });
 
 };
--- middleware/log-request.js
+++ middleware/log-request.js
@@ -1,13 +1,12 @@
 'use strict';
 
-const config = require('../common/config');
 const log = require('../common/log')('idearium-lib:middleware:log-request');
 
 module.exports = function logRequest (req, res, next) {
 
+    const config = require('../common/config');
     const exclude = config.get('logRequestExclude') || ['admin/public', 'ping'];
     const regex = new RegExp(`^/${exclude.join('|')}/?.*$`);
-    const start = Date.now();
 
     // Don't do any of this, if the regex matches.
     if (regex.test(req.url)) {
@@ -18,30 +17,16 @@ module.exports = function logRequest (req, res, next) {
     next();
 
     // Set the response start.
-    req.responseStart = req.responseStart || start;
+    req.responseStart = req.responseStart || req.responseStart || Date.now();
 
     // When the request is finished, log it.
     res.on('finish', () => {
 
-        const duration = Date.now() - start;
-        const packet = {
-            production: config.get('production'),
+        log.debug({
+            duration: Date.now() - start,
             req,
-            responseTime: duration,
-            status: this.statusCode,
-        };
-
-        // Log a short version of the user object, if it exists.
-        if (req.user && req.user.username && req.user._id) {
-
-            packet.user = {
-                id: req.user._id,
-                username: req.user.username,
-            };
-
-        }
-
-        log.debug(packet);
+            res,
+        });
 
     });
 

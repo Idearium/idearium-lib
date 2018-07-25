f2209e5c56e48da8ddfef7a28cfa61bb1b147d87
Allan Chau <allan.chau@icloud.com>
2018-07-25 11:48:44 +0930

    Update middleware

--- middleware/log-request.js
+++ middleware/log-request.js
@@ -1,15 +1,13 @@
 'use strict';
 
-var bunyan = require('bunyan');
+const config = require('../common/config');
+const log = require('../common/log')('idearium-lib:middleware:log-request');
 
-module.exports = function (req, res, next) {
+module.exports = function logRequest (req, res, next) {
 
-    // Load the configuration, and an instance of Logger.
-    const config = require('../common/config'),
-        Logger = require('../lib/logs/logger'),
-        exclude = config.get('logRequestExclude') || ['admin\/public', 'ping'],
-        regex = new RegExp('^\/' + exclude.join('|') + '\/?.*$'),
-        start = Date.now();
+    const exclude = config.get('logRequestExclude') || ['admin/public', 'ping'];
+    const regex = new RegExp(`^/${exclude.join('|')}/?.*$`);
+    const start = Date.now();
 
     // Don't do any of this, if the regex matches.
     if (regex.test(req.url)) {
@@ -19,46 +17,31 @@ module.exports = function (req, res, next) {
     // Let's move on straight away
     next();
 
-    // Create an instance of the logger.
-    const log = new Logger({
-        name: 'request',
-        context: 'idearium-lib:middleware:log-request',
-        level: 'info',
-        stdErr: config.get('logToStdout') == undefined ? false : config.get('logToStdout'),
-        token: config.get('logEntriesToken') || '',
-        remote: (config.get('logLocation') || '').toLowerCase() === 'remote',
-        local: (config.get('logLocation') || '').toLowerCase() === 'local',
-        serializers: {
-            req: bunyan.stdSerializers.req,
-            res: bunyan.stdSerializers.res,
-            err: bunyan.stdSerializers.err
-        }
-    });
-
     // Set the response start.
     req.responseStart = req.responseStart || start;
 
     // When the request is finished, log it.
-    res.on('finish', function () {
+    res.on('finish', () => {
 
-        let duration = Date.now() - start,
-            packet = {
-                req: req,
-                production: config.get('production'),
-                status: this.statusCode,
-                responseTime: duration
-            };
+        const duration = Date.now() - start;
+        const packet = {
+            production: config.get('production'),
+            req,
+            responseTime: duration,
+            status: this.statusCode,
+        };
 
         // Log a short version of the user object, if it exists.
         if (req.user && req.user.username && req.user._id) {
+
             packet.user = {
+                id: req.user._id,
                 username: req.user.username,
-                id: req.user._id
             };
+
         }
 
-        // Log the data
-        log.info(packet);
+        log.debug(packet);
 
     });
 
